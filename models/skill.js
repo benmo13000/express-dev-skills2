d8c6a5de6edadf5018ff5172cfc4df7a53eb7a63
benmo13000 <benmo13000@gmail.com>
2023-10-06 18:34:00 -0700

    ok.. Now i'm done last commit i forgot to add the checkbox

--- models/skill.js
+++ models/skill.js
@@ -15,30 +15,28 @@ const skills = [
   function update(id, updateSkill) {
     id = parseInt(id);
     const skill = skills.find(skill => skill.id === id);
+    Object.assign(skill, updateSkill);
   }
 
   function deleteOne(id) {
     id = parseInt(id);
+
     const idx = skills.findIndex(skill => skill.id === id);
     skills.splice(idx, 1);
   }
 
   function create(skill) {
+
     skill.id = Date.now() % 1000000;
-    skill.done = false;
+
     skills.push(skill);
   }
 
 	function getOne(id) {
-    // URL params are strings - convert to a number
     id = parseInt(id);
-    // The Array.prototype.find iterator method is
-    // ideal for finding objects within an array
     return skills.find(skill => skill.id === id);
   }
 
-  
-  //code this out maybe
   function getAll() {
     return skills;
   }
